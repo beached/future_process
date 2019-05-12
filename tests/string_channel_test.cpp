75919635f2e96b50d2ba4e820e9bf1520890dc73
Darrell Wright <dwright-git@dawdevel.ca>
2019-05-12 18:12:56 -0400

    .

--- tests/string_channel_test.cpp
+++ tests/string_channel_test.cpp
@@ -3,14 +3,14 @@
 // Copyright (c) 2019 Darrell Wright
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
-// of this software and associated documentation files( the "Software" ), to deal
-// in the Software without restriction, including without limitation the rights
-// to use, copy, modify, merge, publish, distribute, sublicense, and / or sell
-// copies of the Software, and to permit persons to whom the Software is
+// of this software and associated documentation files( the "Software" ), to
+// deal in the Software without restriction, including without limitation the
+// rights to use, copy, modify, merge, publish, distribute, sublicense, and / or
+// sell copies of the Software, and to permit persons to whom the Software is
 // furnished to do so, subject to the following conditions:
 //
-// The above copyright notice and this permission notice shall be included in all
-// copies or substantial portions of the Software.
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
 //
 // THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 // IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
@@ -21,28 +21,32 @@
 // SOFTWARE.
 
 #include <cassert>
-
+#include <string>
+#include <string_view>
 
 #include <daw/daw_benchmark.h>
 
-#include "daw/daw_string_channel.h"
 #include "daw/daw_process.h"
+#include "daw/daw_string_channel.h"
 
 extern bool can_run;
 bool can_run = true;
 
 int main( ) {
 	auto chan = daw::process::string_channel( );
-	static std::string const message = "This is a long string test, how about that eh! Hello World.";
+	static constexpr std::string_view message =
+	  "This is a long string test, how about that eh! Hello World.";
 
-	auto proc = daw::process::fork_process( [&chan]( unsigned int t ) {
-		while( can_run ) {
-			puts( "child: sleeping\n" );
-			sleep( t );
-			puts( "child: awake\n" );
-			chan.write( message );
-		}
-	}, 2 );
+	auto proc = daw::process::fork_process(
+	  [&chan]( unsigned int t ) {
+		  while( can_run ) {
+			  puts( "child: sleeping\n" );
+			  sleep( t );
+			  puts( "child: awake\n" );
+			  chan.write( message );
+		  }
+	  },
+	  2 );
 
 	while( can_run ) {
 		puts( "parent: awaiting child\n" );
